0d3cf5ce5fb2d0cfcc2851df4d09385bd70b5dfa
Tim <tim@the-waite.com>
2023-12-06 21:32:19 -0600

    Day 06 - Part 2

--- 2023/day-06/rust/src/main.rs
+++ 2023/day-06/rust/src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs;
 
+use regex::Regex;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,12 +13,14 @@ fn main() {
     let input = fs::read_to_string(&args[1]).expect("Something went wrong reading the file");
 
     let result = get_multiples_of_races(input.as_str());
+    let single_result = get_single_race(input.as_str());
 
-    println!("Result: {}", result);
+    println!("Result of multiples: {}", result);
+    println!("Result of single: {}", single_result);
 }
 
-fn get_multiples_of_races(input: &str) -> u32 {
-    let re = regex::Regex::new(r"Time:\s+(?P<times>.+?)\s+Distance:\s+(?P<distances>.+)").unwrap();
+fn get_multiples_of_races(input: &str) -> u64 {
+    let re = Regex::new(r"Time:\s+(?P<times>.+?)\s+Distance:\s+(?P<distances>.+)").unwrap();
 
     let captures = re.captures(input).unwrap();
     let times = captures
@@ -24,31 +28,52 @@ fn get_multiples_of_races(input: &str) -> u32 {
         .unwrap()
         .as_str()
         .split_whitespace()
-        .map(|t| t.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+        .map(|t| t.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
 
     captures
         .name("distances")
         .unwrap()
         .as_str()
         .split_whitespace()
-        .map(|t| t.parse::<u32>().unwrap())
+        .map(|t| t.parse::<u64>().unwrap())
         .zip(times)
         .map(|(d, t)| Record::new(t, d).count_ways_to_win())
         .fold(1, |acc, n| acc * n)
 }
 
+fn get_single_race(input: &str) -> u64 {
+    let re = Regex::new(r"Time:\s+(?P<time>.+?)\s+Distance:\s+(?P<distance>.+)").unwrap();
+    let captures = re.captures(input).unwrap();
+    let time = captures
+        .name("time")
+        .unwrap()
+        .as_str()
+        .replace(" ", "")
+        .parse::<u64>()
+        .unwrap();
+    let distance = captures
+        .name("distance")
+        .unwrap()
+        .as_str()
+        .replace(" ", "")
+        .parse::<u64>()
+        .unwrap();
+
+    Record::new(time, distance).count_ways_to_win()
+}
+
 struct Record {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 
 impl Record {
-    fn new(time: u32, distance: u32) -> Self {
+    fn new(time: u64, distance: u64) -> Self {
         Self { time, distance }
     }
 
-    fn count_ways_to_win(&self) -> u32 {
+    fn count_ways_to_win(&self) -> u64 {
         let mut count = 0;
 
         for i in 1..self.time {
@@ -73,15 +98,25 @@ mod tests {
         Distance:  9  40  200",
         288
     )]
-    fn test_count_ids_of_valid_games(#[case] input: &str, #[case] expected: u32) {
+    fn test_get_multiples_of_races(#[case] input: &str, #[case] expected: u64) {
         assert_eq!(crate::get_multiples_of_races(input), expected);
+    }
+
+    #[rstest]
+    #[case(
+        "Time:      7  15   30
+        Distance:  9  40  200",
+        71503
+    )]
+    fn test_get_single_race(#[case] input: &str, #[case] expected: u64) {
+        assert_eq!(crate::get_single_race(input), expected);
     }
 
     #[rstest]
     #[case(7, 9, 4)]
     #[case(15, 40, 8)]
     #[case(30, 200, 9)]
-    fn test_count_ways_to_win(#[case] time: u32, #[case] distance: u32, #[case] expected: u32) {
+    fn test_count_ways_to_win(#[case] time: u64, #[case] distance: u64, #[case] expected: u64) {
         assert_eq!(
             crate::Record::new(time, distance).count_ways_to_win(),
             expected
