a1b2e1245d588c8fe2f7a45d3353b95efb3157ca
Tim Waite <tim@kodex.us>
2024-12-02 11:40:52 -0600

    A bit of cleanup for 12/2/24

--- 2024/day-02/typescript/main.ts
+++ 2024/day-02/typescript/main.ts
@@ -3,25 +3,20 @@ import * as fs from "node:fs";
 export function testReport(report: string) {
   const values = report.split(" ").map(Number);
 
-  const isIncreasing = values.every((val, idx) =>
-    idx + 1 < values.length ? val < values[idx + 1] : true
-  );
+  const isIncreasing = values[1] < values[2];
 
-  const isDecreasing = values.every((val, idx) =>
-    idx + 1 < values.length ? val > values[idx + 1] : true
-  );
-
-  const onlyVariesByOneToThree = values.every((val, idx) => {
-    if (idx + 1 < values.length) {
-      const varies = Math.abs(val - values[idx + 1]);
-
-      return varies > 0 && varies < 4;
+  return values.every((val, idx, arr) => {
+    if (idx + 1 === values.length) {
+      return true;
     }
 
-    return true;
-  });
+    const variance = Math.abs(val - arr[idx + 1]);
 
-  return (isIncreasing || isDecreasing) && onlyVariesByOneToThree;
+    const isMonotonic = isIncreasing ? val < arr[idx + 1] : val > arr[idx + 1];
+    const varianceIsWithinTolerance = variance < 4 && variance > 0;
+
+    return isMonotonic && varianceIsWithinTolerance;
+  });
 }
 
 function main() {
